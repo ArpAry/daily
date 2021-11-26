286a6c0c40d1a45799c87ee3f3bc819e3128e885
ArpAry <arpitarya3299@gmail.com>
2021-11-26 23:44:52 +0530

    hello

---
+++ Java StdinandStdoutI.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+ class Solution{
+    public static void main( String[] args)
+{
+    Scanner s=new Scanner(System.in);
+    int a,b,c;
+    a=s.nextInt();
+    b=s.nextInt();
+    c=s.nextInt();
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+}
+}
