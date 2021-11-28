17acf4fd405adb7d815c4e12cc5b3b4625b92841
ArpAry <arpitarya3299@gmail.com>
2021-11-28 14:41:32 +0530

    cnxkmv

---
+++ Java Stdin and Stdout II/src/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class solution {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double a =scan.nextDouble();
+        scan.nextLine();
+        String ar;
+        ar=scan.nextLine();
+
+        // Write your code here.
+
+      System.out.println("String: "+ar);
+        //System.out.println("%c"+ar);
+         System.out.println("Double: " + a);
+         System.out.println("Int: "  +i);
+    }
+}
