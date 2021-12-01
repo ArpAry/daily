5a4a4887a802999131d7c2ec7de5774ea6af01c6
ArpAry <arpitarya3299@gmail.com>
2021-12-01 23:18:04 +0530

    dsaoijf

---
+++ javaloop1/src/javaloop1/solution.java
@@ -0,0 +1,27 @@
+package javaloop1;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class solution {
+    public static void main(String[] args)  {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int i;
+        for(i=1;i<11;i++)
+        {
+            System.out.printf("%d x %d = %d\n",a,i,a*i);
+        }
+    
+    
+        
+    }
+}
+//package javaloop1;
+
