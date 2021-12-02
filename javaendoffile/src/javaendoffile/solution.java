2a58c2b0d9ab257e7265aa2b7c06fdaa76d15828
ArpAry <arpitarya3299@gmail.com>
2021-12-02 23:01:39 +0530

    hfdf

---
+++ javaendoffile/src/javaendoffile/solution.java
@@ -0,0 +1,18 @@
+package javaendoffile;
+import java.util.*;
+//import java.text.*;
+//import java.math.*;
+//import java.util.regex.*;
+
+public class solution {
+	public static void main(String[] args) {
+        int i=1;
+        Scanner sc=new Scanner(System.in);
+       while(sc.hasNext())
+        {
+            System.out.println(i +" "+sc.nextLine());
+            i++;
+        }
+        sc.close();
+    }
+}
