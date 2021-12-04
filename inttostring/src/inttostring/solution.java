d7fd11c07c39d46b77c3cf08eaf1d094212cce5f
ArpAry <arpitarya3299@gmail.com>
2021-12-05 01:22:17 +0530

    5dec

---
+++ inttostring/src/inttostring/solution.java
@@ -0,0 +1,16 @@
+package inttostring;
+import java.util.*;
+
+
+public class solution {
+	public static void main(String[] args) {
+
+Scanner sc=new Scanner(System.in);
+int a;
+a=sc.nextInt();
+String ar;
+ar=a+"";
+System.out.println(ar);
+		 
+	}
+}
