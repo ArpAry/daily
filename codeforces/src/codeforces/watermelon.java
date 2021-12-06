f7a1b148534c7658220a2f76ba747389563bb03c
ArpAry <arpitarya3299@gmail.com>
2021-12-07 01:35:15 +0530

    sd

---
+++ codeforces/src/codeforces/watermelon.java
@@ -0,0 +1,18 @@
+package codeforces;
+	import java.util.*;
+	public class watermelon{
+	 public static void  main(String[] args)  
+	{
+	      int n;
+	   Scanner sc=new Scanner(System.in);
+	   n=sc.nextInt();
+	   if(n%2==0&&n!=2)
+	   {
+	      System.out.println("YES");
+	   }
+	   else
+	   {
+	      System.out.println("NO");
+	   }
+	}
+	}
