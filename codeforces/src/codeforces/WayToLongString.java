2d268d5e3467901f946d44fab93f4299d4b2ff4c
ArpAry <arpitarya3299@gmail.com>
2021-12-09 02:08:47 +0530

    kcnk

--- codeforces/src/codeforces/WayToLongString.java
+++ codeforces/src/codeforces/WayToLongString.java
@@ -1,31 +1,31 @@
 package codeforces;
 import java.util.*;
-
 public class WayToLongString {
+	   public static void main(String[] args )
+	   {
+	      Scanner in=new Scanner(System.in);
+	      int n,i;
+	      n=in.nextInt();
+	      
+	      String ar;
+	      String arr=in.nextLine();
+	      for(i=0;i<n;i++)
+	      {
+	         ar=in.nextLine();
+	         int len=ar.length();
+	         if(len>10)
+	        {
+	            int k=len-2;
+	            System.out.println(""+ar.charAt(0)+k+ar.charAt(len-1));
+	         }
+	         else
+	         {
+	            System.out.println(ar );
+	         }
+	         //System.out.println(len);
+	      }
+	      
+	   }
 
-	public static void main(String[] args) {
-		
-		      
-		      Scanner sc=new Scanner(System.in);
-		      int n=sc.nextInt();
-		      for(int i=0;i<n;i++)
-		      {
-		    	  
-		         String str=sc.next();
-		         int l=str.length();
-		         if(l>10)
-		         {
-		            System.out.println(""+str.charAt(0)+(l-2)+str.charAt(l-1));
-		         }
-		         else
-		         {
-		            System.out.println(str);
-		         }
-		      }
-		      
-		   }
-}	
 
-	
-
-
+}
