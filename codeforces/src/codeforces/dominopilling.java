d9292cc75d603b3180e1b4c598f7702d00554c5c
Arpit Arya <87825317+ArpAry@users.noreply.github.com>
2021-12-12 14:56:53 +0530

    Add files via upload

---
+++ codeforces/src/codeforces/dominopilling.java
@@ -0,0 +1,14 @@
+package codeforces;
+import java.util.*;
+public class dominopilling {
+
+	public static void main(String[] args)
+	{
+		int m,n,a;
+		Scanner in=new Scanner(System.in);
+		m=in.nextInt();
+		n=in.nextInt();
+		a=m*n/2;
+		System.out.println(a);
+	}
+}
