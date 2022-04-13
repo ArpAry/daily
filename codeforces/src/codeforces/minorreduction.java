36493b1ae5abe384654c727630ad489c870f5ee5
ArpAry <arpitarya3299@gmail.com>
2022-04-13 17:50:31 +0530

    minor reduction

--- codeforces/src/codeforces/minorreduction.java
+++ codeforces/src/codeforces/minorreduction.java
@@ -10,10 +10,22 @@ public static void main(String [] args)
 	{
 		String s1=in.nextLine();
 		int n1=s1.length();
-		for(int i=0;i<n1-1;i++)
+		for(int i=n1-1;i>0;i--)
 		{
-			
-			
+			int max=s1.charAt(i)-48+s1.charAt(i-1)-48;
+			//System.out.println(max);
+			if(max>=10)
+			{
+				//System.out.print(i);
+				System.out.println(s1.substring(0,i-1)+max+s1.substring(i+1,n1));
+				break;
+			}
+			if(i==1&&max<10)
+			{
+			//	System.out.print("no");
+			//	System.out.println(max);
+				System.out.println(max+s1.substring(2,n1));
+			}	
 		}
 		t--;
 	}
