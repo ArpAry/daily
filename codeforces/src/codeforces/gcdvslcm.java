35742e7f3f4912e17f661e614439aa4199d00a08
ArpAry <arpitarya3299@gmail.com>
2022-04-21 00:06:05 +0530

    lcm vs gcf

---
+++ codeforces/src/codeforces/gcdvslcm.java
@@ -0,0 +1,15 @@
+package codeforces;
+import java.util.*;
+public class gcdvslcm {
+	public static void main(String args[] )
+	{
+		Scanner in =new Scanner(System.in);
+		int t=in.nextInt();
+		while(t>0)
+		{
+			int n=in.nextInt();
+			System.out.println((n-3)+" 1 1 1");
+			t--;
+		}
+	}
+}
