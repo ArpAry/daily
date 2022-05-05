151ddc5d28cab5080647e1344b145aba25c6d53b
ArpAry <arpitarya3299@gmail.com>
2022-05-05 18:42:31 +0530

    Roof Construction question

--- codeforces/src/codeforces/RoofConstruction.java
+++ codeforces/src/codeforces/RoofConstruction.java
@@ -1,30 +1,34 @@
 package codeforces;
 import java.util.*;
 public class RoofConstruction {
-	public static void main(String []args)
-	{
-		Scanner in=new Scanner(System.in);
-		int t=in.nextInt();
-		//int ar[]=new int[t];
-		for(int i=0;i<t;i++)
-		{
-			int j=in.nextInt();
-			if(j%2==0)
-			{
-				for(int k=1;k<j;k++)
-				{
-					System.out.print(k );
-				}
-			}
-			else
-			{
-				for(int k=0;k<j;k++)
-				{
-					System.out.println(k);
-				}
-			}
-			System.out.println();
-		}
+	
+	public static void main(String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    int t=in.nextInt();
+	    while(t>0)
+	    {
+	        int n=in.nextInt();
+	        int c=0,a=n-1;
+	        while(a!=0)
+	        {
+	            a/=2;
+	            c++;
+	        }
+	        int b=(int)Math.pow(2,c-1);
+	       // System.out.println(b);
+	        
+	        for(int i=b-1;i>=0;i--)
+	        {
+	            System.out.print(i+" " );
+	        }
+	        for(int i=b;i<=n-1;i++)
+	        {
+	            System.out.print(i+" ");
+	        }
+	        System.out.println();
+	        t--;
+	    }
+	//	System.out.println("Hello World");
 	}
 
 }
