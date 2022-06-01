6fffcc92c4ebb8e453f47cd72b517866f42a01a9
ArpAry <arpitarya3299@gmail.com>
2022-06-01 22:31:31 +0530

    adding one file

--- codeforces/src/codeforces/ShoeShuffling.java
+++ codeforces/src/codeforces/ShoeShuffling.java
@@ -1,6 +1,6 @@
 package codeforces;
 import java.util.*;
-public class ShoeShuffling {
+class ShoeShuffling{
 	public static void main(String []args)
 	{
 		Scanner in=new Scanner(System.in);
@@ -9,75 +9,39 @@ public class ShoeShuffling {
 		{
 			int n=in.nextInt();
 			int ar[]=new int[n];
-			int count=0;
+			int ct[]=new int[n];
 			for(int i=0;i<n;i++)
 			{
-				ar[i]=in.nextInt();	
+				ar[i]=in.nextInt();
 			}
-			
-
+			int k=0,count=1;
 			for(int i=0;i<(n-1);i++)
 			{
-				if(ar[i]==ar[i+1])
+				if(ar[i]==ar[i+1]){
+					count++;
+					ct[k]=count;
+				}
+//				if(k==0&&count>0)
+//					{
+//						ct[k]=count;
+//					}
+				else {
+					ct[k]=count;
+					k++;
 					count=1;
-				else
-				{
-					count=0;
-					break;
 				}
-					
 			}
-			
-			if(count==0)
+			int j=k;
+			while(j>=0)
 			{
-//					if(n==1)
-//						System.out.println(-1);
-//					if(n==2)
-//					{
-//						if(ar[0]==ar[1])
-//							System.out.println(ar[0]+" "+ar[1]);
-//						else
-//							System.out.println(-1);
-//					}
-//					if(n==3)
-//					{
-//						if(ar[0]==ar[1]&&ar[1]==ar[2])
-//							System.out.println(ar[0]+" "+ar[1]+" "+ar[2]);
-//						else
-//							System.out.println(-1);
-//					}
-				if(n<4)
-					System.out.println(-1);
-				else if(n>=4)
-				{
-					if(ar[0]==ar[1]&&ar[n-1]==ar[n-2])
-					{
-						System.out.print(2+" "+1+" ");
-						for(int i=3;i<=n-2;i++)
-						{
-							System.out.print((i)+" ");
-						}
-						System.out.print(n+" "+(n-1));
-						
-					}
-					else
-					{
-						System.out.println(-1);
-					}
-				}
-				
+				System.out.println("case "+ct[j]);
+				j--;
+				//ar[i]=in.nextInt();
 			}
-			else
-			{
-				System.out.print(n+" ");
-				for(int i=1;i<=n-1;i++)
-				System.out.print(i+" ");
-			}
-			System.out.println();
 			
-			
-			
+		
 			t--;
 		}
 	}
+	
 }
