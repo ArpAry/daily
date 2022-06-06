cfdcc82e58eceeedcb6694ab1812bc7cea8677ab
ArpAry <arpitarya3299@gmail.com>
2022-06-07 00:29:26 +0530

    submit the  dorms war question

--- codeforces/src/codeforces/DormsWar.java
+++ codeforces/src/codeforces/DormsWar.java
@@ -1,37 +1,95 @@
 package codeforces;
+
 import java.util.*;
-public class DormsWar {
-	public static void main(String []args)
-	{
-		Scanner in=new Scanner (System.in);
-		int t=in.nextInt();
-		while(t>0)
-		{
-			int n=in.nextInt();
-			in.nextLine();
-			String s1=in.nextLine();
-			//in.nextLine();
-			int k=in.nextInt();
-			char ar[]=new char[k];
-			for(int i=0;i<k;i++)
-				ar[i]=in.next().charAt(0);
-//			for(int i=0;i<k;i++)
-//				System.out.println(ar[i]);
-			int ans=0,last=-1;
-			for(int i=n-1;i>=0;i--)
-			{
-				if(last!=-1)
-				ans=Math.max(last-i, ans);
-					for(int j=0;j<k;j++)
-					{
-						if(s1.charAt(i)==ar[j])
-						{	last=i;
-						//System.out.println(last+" "+i );
-						}
-					}	
-			}
-			System.out.println(ans);
-			t--;
+import java.io.*;
+
+public class DormsWar{
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br=new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str=br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+		private final BufferedWriter bw;
+
+		public FastWriter() {
+			this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		}
+
+		public void print(Object object) throws IOException {
+			bw.append("" + object);
+		}
+
+		public void println(Object object) throws IOException {
+			print(object);
+			bw.append("\n");
+		}
+
+		public void close() throws IOException {
+			bw.close();
 		}
 	}
+    public static void main(String[] args) {
+        try {
+            FastReader in=new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases=in.nextInt();
+            while(testCases-- > 0){
+                // write code here
+            	int n=in.nextInt();
+    			String s1=in.next();
+    			//in.nextLine();
+    			int k=in.nextInt();
+    			Set <Character> set=new HashSet <>();
+    			for(int i=0;i<k;i++)
+    				set.add(in.next().charAt(0));
+//    			for(int i=0;i<k;i++)
+//    				System.out.println(ar[i]);
+    			int prev=0,operation=0;
+    			for(int i=0;i<n;i++)
+    			{
+    				if(set.contains(s1.charAt(i)))
+    				{
+    					operation=Math.max(operation,i-prev);
+    					prev=i;
+    				}
+    			}
+    			out.println(operation);
+    		//	t--;
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
 }
